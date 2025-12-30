afe1a958de00abbd27c030b23893a316b3a547bf
René Jossen <rene@enjor.ch>
2025-12-30 13:37:32 +0100

    Add global settings fields for contact information and options page

---
+++ global-settings/setup.php
@@ -0,0 +1,150 @@
+<?php
+
+add_action( 'acf/include_fields', function() {
+	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+		return;
+	}
+
+	acf_add_local_field_group( array(
+	'key' => 'group_67ac9688cb615',
+	'title' => 'Kontaktdaten',
+	'fields' => array(
+		array(
+			'key' => 'field_67ac9688b04a0',
+			'label' => 'Anschrift',
+			'name' => 'anschrift',
+			'aria-label' => '',
+			'type' => 'textarea',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'allow_in_bindings' => 0,
+			'rows' => '',
+			'placeholder' => '',
+			'new_lines' => 'br',
+		),
+		array(
+			'key' => 'field_67ac97c59d02b',
+			'label' => 'Bürozeiten',
+			'name' => 'burozeiten',
+			'aria-label' => '',
+			'type' => 'textarea',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'allow_in_bindings' => 0,
+			'rows' => '',
+			'placeholder' => '',
+			'new_lines' => '',
+		),
+		array(
+			'key' => 'field_67ac96e4b04a1',
+			'label' => 'E-Mail',
+			'name' => 'e-mail',
+			'aria-label' => '',
+			'type' => 'email',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '33',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'allow_in_bindings' => 0,
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+		),
+		array(
+			'key' => 'field_67ac96fcb04a2',
+			'label' => 'Telefon',
+			'name' => 'telefon',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '33',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'allow_in_bindings' => 0,
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+		),
+		array(
+			'key' => 'field_67ac97e79d02c',
+			'label' => 'LinkedIn',
+			'name' => 'linkedin',
+			'aria-label' => '',
+			'type' => 'url',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '33',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'allow_in_bindings' => 0,
+			'placeholder' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'options_page',
+				'operator' => '==',
+				'value' => 'globale-optionen',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+	'display_title' => '',
+) );
+} );
+
+add_action( 'acf/init', function() {
+	acf_add_options_page( array(
+	'page_title' => 'Globale Optionen',
+	'menu_slug' => 'globale-optionen',
+	'menu_title' => 'Globales',
+	'position' => 2,
+	'redirect' => false,
+	'menu_icon' => array(
+		'type' => 'dashicons',
+		'value' => 'dashicons-visibility',
+	),
+	'icon_url' => 'dashicons-visibility',
+) );
+} );
+
